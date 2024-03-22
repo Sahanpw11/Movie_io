e20d297093dfbab66f0f9fdd2396a3dfbbb658a5
Sahan Wijesundara <sahanwijesundara11@gmail.com>
2024-03-22 10:57:23 +0530

    name

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'movie_io'),
+      home: const MyHomePage(title: 'movie_io for life'),
     );
   }
 }
